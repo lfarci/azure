ee2e87c917ce956b74aa9bea1ac17bc573bff335
Logan Farci <farci.logan@gmail.com>
2024-03-09 15:07:56 +0100

    Add Product model

---
+++ src/Shop/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Shop.Models
+{
+    public record Product
+    {
+        public string Name { get; init; } = string.Empty;
+        public int Amount { get; init; }
+        public double Price { get; init; }
+    }
+}
