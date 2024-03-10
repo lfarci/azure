33fcaf7768c34eddbfbe6b8763cfb8d4417ce319
Logan Farci <farci.logan@gmail.com>
2024-03-10 17:22:46 +0100

    Parses the client principal JWT and show it to the screen

---
+++ src/Shop/Models/Profile.cs
@@ -0,0 +1,11 @@
+﻿namespace Shop.Models
+{
+    public record Profile
+    {
+        public string? IdToken { get; init; }
+        public string? AccessToken { get; init; }
+        public string? ExpiresOn { get; init; }
+        public string? RefreshToken { get; init; }
+        public string? Name { get; internal set; }
+    }
+}
--- src/Shop/Pages/Index.cshtml.cs
+++ src/Shop/Pages/Index.cshtml.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Shop.Models;
+using Shop.Services;
+using System.Security.Claims;
 
 namespace Shop.Pages
 {
@@ -12,9 +15,21 @@ namespace Shop.Pages
             _logger = logger;
         }
 
+        public Profile Profile { get; set; }
+        public ClaimsPrincipal? Principal { get; private set; }
+
         public void OnGet()
         {
+            Profile = new Profile
+            {
+                IdToken = Request.Headers["X-MS-TOKEN-AAD-ID-TOKEN"],
+                AccessToken = Request.Headers["X-MS-TOKEN-AAD-ACCESS-TOKEN"],
+                ExpiresOn = Request.Headers["X-MS-TOKEN-AAD-EXPIRES-ON"],
+                RefreshToken = Request.Headers["X-MS-TOKEN-AAD-REFRESH-TOKEN"],
+                Name = ClaimsPrincipal.Current?.FindFirst(ClaimTypes.Name)?.Value,
+            };
 
+            Principal = ClaimsPrincipalParser.Parse(Request);
         }
     }
 }
