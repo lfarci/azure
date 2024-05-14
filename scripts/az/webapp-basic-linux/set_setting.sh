133ede5ba0d5b3c948d382a567611219c871f0f8
Logan Farci <31938998+lfarci@users.noreply.github.com>
2024-05-14 08:35:43 +0200

    Add commands for settings in bulk

--- scripts/az/webapp-basic-linux/set_setting.sh
+++ scripts/az/webapp-basic-linux/set_setting.sh
@@ -2,6 +2,8 @@
 
 applicationName="$applicationName-webapp"
 
+# App settings commands
+
 az webapp config appsettings list \
     --name $applicationName \
     --resource-group $resourceGroupName
@@ -11,12 +13,16 @@ az webapp config appsettings set \
     --resource-group $resourceGroupName \
     --settings ValueA=Coucou ValueB=Bonjour ValueC=Salut
 
+az webapp config appsettings set \
+    --resource-group $resourceGroupName \
+    --name $applicationName \
+    --settings "@settings.json"
+
+# Connection string commands
+
 az webapp config connection-string set \
     --resource-group basic-linux-webapp-rg \
     --name myapp-20240513233105-webapp \
     --settings SqlServer=ThisIsATest \
     --connection-string-type Custom
 
-az webapp config appsettings list \
-    --name $applicationName \
-    --resource-group $resourceGroupName
