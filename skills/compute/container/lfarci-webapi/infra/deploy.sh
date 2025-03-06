cc498b1e02f66ba2068d1c6227498efd92c6be18
Logan Farci <farci.logan@gmail.com>
2025-03-06 08:55:29 +0100

    Fix variable assignment syntax and add ACR creation in deploy script

--- skills/compute/container/lfarci-webapi/infra/deploy.sh
+++ skills/compute/container/lfarci-webapi/infra/deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-$resource_group_name = "lfarci-container-webapi-rg"
-$location = "germanywestcentral"
+resource_group_name="lfarci-container-webapi-rg"
+location="germanywestcentral"
+acr_name="lfarciacr$(( RANDOM % 1000 ))"
 
 az group create --name $resource_group_name --location $location
-
+az acr create --resource-group $resource_group_name --name $acr_name --sku Basic
